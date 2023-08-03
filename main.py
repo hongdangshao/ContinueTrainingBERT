552c2be9e7df878ee2c2ab1e7896387b37b6a9cc
iGangao <18256715510@163.com>
2023-08-03 19:43:44 +0800

    save model in output_path

--- main.py
+++ main.py
@@ -3,14 +3,20 @@
 # @Time : 2023/8/2 19:57
 # @Author : Gangao Liu
 from ContinueTrainingBERT.utils.config import BERTConfig
-from ContinueTrainingBERT.BertForPreTraining.dataset import BERTDataset
+from ContinueTrainingBERT.BertForPreTraining.dataset import BERTDataset as PreDataset
+from ContinueTrainingBERT.BertForMaskedLM.dataset import BERTDataset as MaskedLMDataset
 from torch.utils.data import DataLoader
-from ContinueTrainingBERT.BertForPreTraining.trainer import BERTTrainer
+from ContinueTrainingBERT.BertForPreTraining.trainer import BERTTrainer as PreTraining
+from ContinueTrainingBERT.BertForMaskedLM.trainer import BERTTrainer as MaskedLM
 Config = BERTConfig()
-train_dataset = BERTDataset(Config.path_datasets)
-train_data_loader = DataLoader(train_dataset, batch_size=64, num_workers=5)
-
-Trainer = BERTTrainer(config=Config, train_dataloader=train_data_loader)
+pre_train_dataset = PreDataset(Config.path_datasets)
+lm_train_dataset = MaskedLMDataset(Config.path_datasets)
+pretrain_data_loader = DataLoader(pre_train_dataset, batch_size=64, num_workers=5)
+lm_train_data_loader = DataLoader(lm_train_dataset, batch_size=64, num_workers=5)
+PreTrainer = PreTraining(config=Config, train_dataloader=pretrain_data_loader)
+LMTrainer = MaskedLM(config=Config, train_dataloader=lm_train_data_loader)
 
 for epoch in range(10):
-    Trainer.train(epoch)
+    LMTrainer.train(epoch)
+
+LMTrainer.save()
